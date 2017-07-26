7c794620adc20625944903cf87243715a086aa4b
Juha Myllari <jmyllari@gmail.com>
2017-07-26 20:48:03 +0300

    create Main.java

---
+++ ArithmeticVisualizer/src/main/java/av/arithmeticvisualizer/Main.java
@@ -0,0 +1,24 @@
+package av.arithmeticvisualizer;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        double[][] A = {{1, 2, 3,}, {4, 5, 6}};
+        double[][] B = {{18}};
+        
+        Node root = new AdditionNode(new ArrayNode(7), new ArrayNode(A));
+        
+        try {
+            System.out.println(Arrays.deepToString(root.eval()));
+        } catch (DimensionMismatchException ex) {
+            System.out.println("Evaluation failed with message:");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
